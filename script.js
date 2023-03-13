d7ea677b731d311c02ae48d1671a78f0e6af0e87
Manish-jadav <120002690+Manish-jadav@users.noreply.github.com>
2023-03-14 00:18:56 +0530

    js

--- script.js
+++ script.js
@@ -1,109 +1,156 @@
-function onFormSubmit(){
-    nameValidation();
-    emailValidation();
-    numberValidation();
-    // if((emailValidation == true) && (nameValidation == true) && (numberValidation == true)){
-    // var formData = readFormData();
-    // insertNewRecord(formData);
-    // insertData(formData);
-    // }
-    var formData = readFormData();
-    insertNewRecord(formData);
-    insertData(formData);    
-}
-
-function nameValidation(){
-    var name = document.getElementById('name').value;
-    if(name=="")
-    {
-         document.getElementById("nm").style.color="red";
-        // return false;
-    }      
-}
-
+//  ------------------------- Validation Part -------------------------------------- // 
+let flag =1;
  function emailValidation() 
  {
   var x = document.forms["myForm"]["email"].value;
- 
+
   var atpos = x.indexOf("@");
   var dotpos = x.lastIndexOf(".");
- 
+
     if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) 
     {
         document.getElementById("mail").style.color="red";
-        //return false;
+        return false;
+        
     }
  }
- 
+
 function numberValidation(){
     var a = document.getElementById('number').value;
     if(a==""){
         document.getElementById("no").style.color="red";
-       //return false;
+       return false;
+       
     }
     if(isNaN(a)){
         document.getElementById("no").style.color="red";
         //document.getElementById("star").innerHTML="*Only Numbers are allowed";
-        //return false;
+        return false;
+        
     }
     if((a.length<10)){
         document.getElementById("no").style.color="red";
-       // return false;
+       return false;
+        
     }
 }
-function readFormData(){
+var row = null;
+function Onsubmit() {
+    //emailValidation();
+    //numberValidation();
+    // if((emailValidation() == true)&&(numberValidation() == true)){
+    var dataEntered = retrieveData();
+    var readData = readingDataFromLocalStorage(dataEntered);
+    if (row == null) {
+        insert(readData);
+        msg.innerHTML = "Data Inserted !"
+    }
+    else {
+        update();
+        msg.innerHTML = "Data Updated !"
+    }
+// }
+// else{
+    // alert("Please... fill Currect Detailes  !");
+// }
+}
 
-    var formData = {};
-    formData["name"] = document.getElementById("name").value;
-    formData["email"] = document.getElementById("email").value;
-    formData["number"] = document.getElementById("number").value;
-    formData["date"] = document.getElementById("date").value;
-    formData["time"] = document.getElementById("time").value;
-    formData["color"] = document.getElementById("color").value;
-    formData["radio"] =document.getElementsByClassName("radio").value;
-    formData["slider"] = document.getElementById("slider").value;
-    formData["checkbox"] = document.getElementById("checkbox").value;
-    formData["file"] ="Photo"//document.getElementById("file ").value;
+
+function retrieveData() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var number = document.getElementById("number").value;
+    var date = document.getElementById("date").value;
+    var time = document.getElementById("time").value;
+    var color = document.getElementById("color").value;
+    var radio = document.getElementById("radio").value;
+    var slider = document.getElementById("slider").value;
+    var checkbox = document.getElementById("checkbox").value;
+    var file = document.getElementById("file").value;
+    formData = [name, email, number, date, time, color, radio, slider, checkbox, file];
     return formData;
 }
-function insertNewRecord(data){
-    var table = document.getElementById("empList").getElementsByTagName('tbody')[0];
-    var newRow = table.insertRow(table.length);
-    cell1 = newRow.insertCell(0);
-    cell1.innerHTML = data.name;
-    cell2 = newRow.insertCell(1);
-    cell2.innerHTML = data.email;
-    cell3 = newRow.insertCell(2);
-    cell3.innerHTML = data.number;
-    cell4 = newRow.insertCell(3);
-    cell4.innerHTML = data.date;
-    cell5 = newRow.insertCell(4);
-    cell5.innerHTML = data.time;
-    cell6 = newRow.insertCell(5);
-    cell6.innerHTML = data.color;
-    cell7 = newRow.insertCell(6);
-    cell7.innerHTML = data.radio;
-    cell8 = newRow.insertCell(7);
-    cell8.innerHTML = data.slider;
-    cell9 = newRow.insertCell(8);
-    cell9.innerHTML = data.checkbox;
-    cell10 = newRow.insertCell(9);
-    cell10.innerHTML = data.file;
-    cell11 = newRow.insertCell(10);
-    cell11.innerHTML = `<a href="#">Update</a>
-                        <a href="#">Delete</a>`; // " " ''
+function readingDataFromLocalStorage(dataEntered) {
+    var name = localStorage.setItem("name", dataEntered[0]);
+    var email = localStorage.setItem("email", dataEntered[1]);
+    var number = localStorage.setItem("number", dataEntered[2]);
+    var date = localStorage.setItem("date", dataEntered[3]);
+    var time = localStorage.setItem("time", dataEntered[4]);
+    var color = localStorage.setItem("color", dataEntered[5]);
+    var radio = localStorage.setItem("radio", dataEntered[6]);
+    var slider = localStorage.setItem("slider", dataEntered[7]);
+    var checkbox = localStorage.setItem("checkbox", dataEntered[8]);
+    var file = localStorage.setItem("file", dataEntered[9]);
+
+    // Getting values from local to table
+
+    var name1 = localStorage.getItem("name", name);
+    var email1 = localStorage.getItem("email", email);
+    var number1 = localStorage.getItem("number", number);
+    var date1 = localStorage.getItem("date", date);
+    var time1 = localStorage.getItem("time", time);
+    var color1 = localStorage.getItem("color", color);
+    var radio1 = localStorage.getItem("radio", radio);
+    var slider1 = localStorage.getItem("slider", slider);
+    var checkbox1 = localStorage.getItem("checkbox", checkbox);
+    var file1 = localStorage.getItem("file", file);
+
+    var arr = [name1, email1, number1, date1, time1, color1, radio1, slider1, checkbox1, file1];
+    return arr;
 
 }
-function insertData(formData)
-{
-    var nm=localStorage.setItem("name",formData[0]);
-    var em=localStorage.setItem("email",formData[1]);
-    var no=localStorage.setItem("number",formData[2]);
-    var no=localStorage.setItem("date",formData[3]);
-    var no=localStorage.setItem("time",formData[4]);
-    var no=localStorage.setItem("color",formData[5]);
-    var no=localStorage.setItem("radio",formData[6]);
-    var no=localStorage.setItem("slider",formData[7]);
-    var no=localStorage.setItem("checkbox",formData[8]);
-    var no=localStorage.setItem("file",formData[9]);
+function insert(readData) {
+    //  var table = document.getElementById("table");//.getElementsByTagName('tbody')[0];
+    var row = table.insertRow(table.length);
+    row.insertCell(0).innerHTML = readData[0];
+    row.insertCell(1).innerHTML = readData[1];
+    row.insertCell(2).innerHTML = readData[2];
+    row.insertCell(3).innerHTML = readData[3];
+    row.insertCell(4).innerHTML = readData[4];
+    row.insertCell(5).innerHTML = readData[5];
+    row.insertCell(6).innerHTML = readData[6];
+    row.insertCell(7).innerHTML = readData[7];
+    row.insertCell(8).innerHTML = readData[8];
+    row.insertCell(9).innerHTML = readData[9];
+    row.insertCell(10).innerHTML = `<button onclick = edit(this) style="height: 40px;width: 100px;font-size: 18px;
+    background-color: blue;color:white;border-radius:10px">Edit</button>
+    <button onclick = remove(this) style="height: 40px; width: 100px;font-size: 18px;background-color: red;color:white;border-radius:10px">Delete</button>`;
+}
+
+function edit(td) {
+    row = td.parentElement.parentElement;
+    document.getElementById("name").value = row.cells[0].innerHTML;
+    document.getElementById("email").value = row.cells[1].innerHTML;
+    document.getElementById("number").value = row.cells[2].innerHTML;
+    document.getElementById("date").value = row.cells[3].innerHTML;
+    document.getElementById("time").value = row.cells[4].innerHTML;
+    document.getElementById("color").value = row.cells[5].innerHTML;
+    document.getElementById("radio").value = row.cells[6].innerHTML;
+    document.getElementById("slider").value = row.cells[7].innerHTML;
+    document.getElementById("checkbox").value = row.cells[8].innerHTML;
+    document.getElementById("file").value = row.cells[9].innerHTML;
+
+}
+
+function update() {
+    row.cells[0].innerHTML = document.getElementById("name").value;
+    row.cells[1].innerHTML = document.getElementById("email").value;
+    row.cells[2].innerHTML = document.getElementById("number").value;
+    row.cells[3].innerHTML = document.getElementById("date").value;
+    row.cells[4].innerHTML = document.getElementById("time").value;
+    row.cells[5].innerHTML = document.getElementById("color").value;
+    row.cells[6].innerHTML = document.getElementById("radio").value;
+    row.cells[7].innerHTML = document.getElementById("slider").value;
+    row.cells[8].innerHTML = document.getElementById("checkbox").value;
+    row.cells[9].innerHTML = document.getElementById("file").value;
+    row = null;
+}
+
+function remove(td) {
+    var ans = confirm("Are you sure you want to delete this record ?");
+    if (ans == true) {
+        row = td.parentElement.parentElement;
+        document.getElementById("table").deleteRow(row, rowIndex)
+    }
+
 }
